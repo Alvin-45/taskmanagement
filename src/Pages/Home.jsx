230c0476a9e9902bc641e7f6a39ea79349fc4c94
PranavPradeep390 <pranavpradeep786@gmail.com>
2024-12-16 07:23:03 +0530

    second commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import { Link, useNavigate } from 'react-router'
-
+import { FloatingLabel, Form } from 'react-bootstrap';
 function Home() {
+
+const [userInput, setUserinput]= useState({
+    username: "", password: ""
+  })
     const navigate = useNavigate();
 
     const handleSubmit = () => {
@@ -17,8 +21,13 @@ function Home() {
         } else if (p === "") {
             alert('Enter Password');
         } else {
+            sessionStorage.setItem("existingUser",JSON.stringify(result.data.existingUser))
+             sessionStorage.setItem("firstName",JSON.stringify(result.data.existingUser.firstName))
+             sessionStorage.setItem("email",JSON.stringify(result.data.existingUser.email))
+             sessionStorage.setItem("username",JSON.stringify(result.data.existingUser.username))
+             console.log(result.data.token);
+             sessionStorage.setItem("token",result.data.token)
             localStorage.setItem('username',x)
-            localStorage.setItem('password',p)
             navigate('/dashboard');
         }
     };
@@ -28,14 +37,13 @@ function Home() {
         <div className="coverpage">
             <div className="form">
                 <h4 style={{ color: 'darkblue', backgroundColor: 'white' }}>TASK MANAGEMENT TOOL</h4>
-                <label>
-                    Username: <br />
-                    <input type="text" id="usernme" placeholder="Enter username" />
-                </label>
-                <label>
-                    Password: <br />
-                    <input type="password" id="passwrd" placeholder="Enter Password" />
-                </label>
+                <FloatingLabel controlId="floatingUsername" label="Username" className=' inp'>
+            <Form.Control type="email" placeholder="Username" onChange={e=>setUserinput({...userInput,username:e.target.value})}   style={{backgroundColor:'white',borderTop:'0px',borderBottom:'2px solid white',borderLeft:'0px',borderRight:'0px',textAlign:'center',borderRadius:'0px',color:'black'}}/>
+          </FloatingLabel>
+          <FloatingLabel controlId="floatingPassword" label="Password" className='mb-2 inp'>
+            <Form.Control type="password" placeholder="Password" onChange={e=>setUserinput({...userInput,password:e.target.value})}   style={{backgroundColor:'white',borderTop:'0px',borderBottom:'2px solid white',borderLeft:'0px',borderRight:'0px',textAlign:'center',borderRadius:'0px',color:'black'}}/>
+          </FloatingLabel>
+                
                 <button onClick={handleSubmit}>LOGIN</button>
                 <p style={{ color: 'black', backgroundColor: 'white' }}>
                     Don't have an account? <u style={{ color: 'black', backgroundColor: 'white' }}>Register</u>
